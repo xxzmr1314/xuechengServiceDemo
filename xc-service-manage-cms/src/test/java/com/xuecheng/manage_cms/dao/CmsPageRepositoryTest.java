ba43964be569cdb5090b43a9a2d3a09c693a1d0a
xxzmr1314 <545831783@qq.com>
2019-09-25 17:44:31 +0800

    Initial commit

---
+++ xc-service-manage-cms/src/test/java/com/xuecheng/manage_cms/dao/CmsPageRepositoryTest.java
@@ -0,0 +1,43 @@
+package com.xuecheng.manage_cms.dao;
+
+import com.xuecheng.framework.domain.cms.CmsPage;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class CmsPageRepositoryTest {
+    @Autowired
+    CmsPageRepository cmsPageRepository;
+
+    @Test
+    public void testFindAll(){
+       List<CmsPage> all = cmsPageRepository.findAll();
+        System.out.println(all);
+    }
+
+    //分页查询测试
+    @Test
+    public void testFindPage(){
+        //分页参数
+        int page = 1;
+        int size = 10;
+        Pageable pageable = PageRequest.of(page,size);
+        Page<CmsPage> all = cmsPageRepository.findAll(pageable);
+        System.out.println(all);
+    }
+
+    //修改
+    @Test
+    public void testUpdate(){
+
+    }
+}
